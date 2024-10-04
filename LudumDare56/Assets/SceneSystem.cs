307b7f434bf53dc7d723c35bc8593f9c9e8df23f
Sabrina Cooke <sabrinalcooke@gmail.com>
2024-10-04 14:35:30 +1300

    Basic scene control

---
+++ LudumDare56/Assets/SceneSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSystem : MonoBehaviour
+{
+    private enum Scenes
+    {
+        MainMenu = 0,
+        Game = 1,
+    }
+    
+    public void LoadScene(int sceneIndex)
+    {
+        
+        Debug.Log($"Loading Scene {sceneIndex}: {(Scenes)sceneIndex}");
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
