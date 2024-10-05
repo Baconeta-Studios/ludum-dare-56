2cd8d35390bcf36c8bb8969ed049e4ad39118411
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-10-05 17:18:07 +1300

    Add tests

---
+++ LudumDare56/Assets/_Scripts/Tests/DeckTester.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        CardDeck deck = gameObject.AddComponent<CardDeck>();
+        
+        BlankCard deckCard1 = gameObject.AddComponent<BlankCard>();
+        deckCard1.Initialize(deck, "Card 1");
+        BlankCard deckCard2 = gameObject.AddComponent<BlankCard>();
+        deckCard2.Initialize(deck, "Card 2");
+        BlankCard deckCard3 = gameObject.AddComponent<BlankCard>();
+        deckCard3.Initialize(deck, "Card 3");
+        BlankCard deckCard4 = gameObject.AddComponent<BlankCard>();
+        deckCard4.Initialize(deck, "Card 4");
+        
+        Stack<CardBase> cards = new Stack<CardBase>();
+        cards.Push(deckCard1);
+        cards.Push(deckCard2);
+        cards.Push(deckCard3);
+        cards.Push(deckCard4);
+        deck.Initialize(cards, 0);
+        
+        Debug.Log(deck.Count);
+
+        Debug.Log(deck.DrawPileToString());
+        deck.ReshuffleDiscardToDraw();
+        Debug.Log(deck.DrawPileToString());
+    }
+}
