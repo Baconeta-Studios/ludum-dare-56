1ceea16b136b5129b0a6cf63d28e856077283940
James Coburn <61681076+JamesCoburn246@users.noreply.github.com>
2024-10-06 15:23:40 +1300

    Add game time manager

---
+++ LudumDare56/Assets/_Scripts/Managers/GameTimeManager.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+
+namespace _Scripts.Managers
+{
+    public class GameTimeManager : MonoBehaviour
+    {
+        private bool isRacing;
+        
+        private float totalRaceTimeSoFar; // Total time since the race began, updated each Update().
+        private float lastTimeFinishLineCrossed; // Used to calculate lap times.
+        // Last Lap.
+        private float lastLapTime;
+        private TimeSpan lastLapTimeCache;
+        // Fastest Lap.
+        private float fastestLap;
+        private TimeSpan fastestLapCache;
+        
+        
+        public void StartRaceTimer()
+        {
+            // Reset timer.
+            totalRaceTimeSoFar = 0;
+            lastTimeFinishLineCrossed = 0;
+            lastLapTime = 0;
+            lastLapTimeCache = TimeSpan.Zero;
+            fastestLap = 0;
+            fastestLapCache = TimeSpan.Zero;
+            
+            // Start timer.
+            isRacing = true;
+        }
+
+        public void StopRaceTimer()
+        {
+            isRacing = false;
+        }
+        
+        public TimeSpan GetRaceTime()
+        {
+            return TimeSpan.FromSeconds(fastestLap);
+        }
+
+        public TimeSpan GetLastLapTime()
+        {
+            return lastLapTimeCache;
+        }
+
+        public TimeSpan GetFastestLapTime()
+        {
+            return fastestLapCache;
+        }
+
+        public void TriggerLapEndEvent()
+        {
+            // Calculate last-lap time.
+            lastLapTime = totalRaceTimeSoFar - lastTimeFinishLineCrossed;
+            lastTimeFinishLineCrossed = totalRaceTimeSoFar;
+            
+            // Update fastest-lap time.
+            if (lastLapTime < fastestLap)
+            {
+                fastestLap = lastLapTime;
+            }
+            
+            // Update cached times.
+            lastLapTimeCache = TimeSpan.FromSeconds(lastLapTime);
+            fastestLapCache = TimeSpan.FromSeconds(fastestLap);
+        }
+
+        private void Update()
+        {
+            if (isRacing)
+            {
+                totalRaceTimeSoFar += Time.deltaTime;
+            }
+        }
+    }
+}
