2a106205092ed0db608eec696ae0ff7ebb1f7313
Josh Villgren <joshuavillgren@gmail.com>
2024-10-07 04:01:29 +1300

    some todos

--- LudumDare56/Assets/_Scripts/Cards/Sabotage/SabotageEngine.cs
+++ LudumDare56/Assets/_Scripts/Cards/Sabotage/SabotageEngine.cs
@@ -11,6 +11,8 @@ namespace _Scripts.Cards.Sabotage
         {
             Vector3 position = this.transform.parent.transform.position;
             Instantiate(sabotageObjPrefabRef, position, Quaternion.identity);
+            // TODO will need to parent to the spline unless this is a singleton object not parented to a racer
+            // Will need to have a box collider built in
         }
     }
 }
